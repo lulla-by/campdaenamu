ed3be4865dfc297d08089d2c763841bd5467575d
pisipp <pisipp24@gmail.com>
2022-08-27 00:22:50 +0900

    카드리스트 생성

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import Card from "./Card"
 import Button from "../elem/Button"
 
@@ -6,10 +7,25 @@ function CardList () {
     
     return (
         <>
-        <Card/>
         <Button>글쓰기</Button>
+        <CdListContainer>
+        <Card/>
+        </CdListContainer>
         </>
     )
 }
 
 export default CardList;
+
+const CdListContainer = styled.div`
+    display: block;
+    flex-direction: column;
+    border: 1px solid greenyellow;
+    min-width: 600px;
+    max-width: 1200px;
+    height: 90vh;
+    justify-content: center;
+    background-color: #ffffff;
+    margin: 1em;
+`;
+
