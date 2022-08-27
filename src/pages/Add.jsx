12884c3cbd5602e6c241edea4f74bb47c9ed4fac
limdaeun1 <limdaeun1_@naver.com>
2022-08-27 09:23:00 +0900

    addcard 뷰

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,20 +1,52 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Input from "../elem/Input";
+import Button from "../elem/Button"
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+
 
 function Add () {
+    const history = useHistory();
+    
     return (
        <>
         <Layout>
-            <Input type={"text"} placeholder="안녕하세요"></Input>
-            <Input type={"text"} placeholder="안녕하세요"></Input>
-            <Input type={"text"} placeholder="안녕하세요"></Input>            
+            <h2>작성자</h2>
+            <Input type={"text"} placeholder="작성자의 이름을 입력해주세요."></Input>
+            <h2>제목</h2>
+            <Input type={"text"} placeholder="제목을 입력해주세요."></Input>
+            <h2>내용</h2>
+            <Textarea 
+                name="body"
+                rows="10"
+                placeholder="내용을 입력해주세요."
+            />
+            
+            <Butbox>
+            <Button onClick={() => { history.goBack("/");}}>등록</Button>
+            </Butbox>
         </Layout>
-            <div>
-            <button>이전으로</button><button>등록</button>
-            </div>
         </>
     )
 }
 
 export default Add;
+
+const Textarea = styled.textarea`
+  box-sizing: border-box;
+  width: 100%;
+  outline: none;
+  border-radius: 8px;
+  padding: 12px;
+  font-size: 14px;
+  border: 1px solid gray;
+`;
+
+const Butbox = styled.div`
+    text-align: center;
+    padding-top: 30px;
+    
+`
+
+
