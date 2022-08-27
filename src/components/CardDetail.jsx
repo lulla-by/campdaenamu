04ee113d30fb585403a13a7bf2abc08a3a3dd8c4
yesol_lee <thfl3363@hanmail.net>
2022-08-27 09:45:48 +0900

    디테일페이지 뷰 구성

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import AddComments from "../comments/AddComments";
+import Commentlist from "../comments/Commentlist";
+import styled from "styled-components";
+import Button from "../elem/Button";
 
-function CardDetail () {
+function CardDetail() {
     return (
-       <>
-        <div>
-            <h3>닉네임</h3>
-            <h3>title</h3>
-            <h3>desc</h3>
-        </div>
-        <button>수정하기</button>
-        <button>삭제하기</button>
-        </> 
+        <>
+            <DetailBox>
+                <h3>닉네임</h3>
+                <h3>title</h3>
+                <h3>desc</h3>
+                <Button>수정하기</Button>
+                <Button>삭제하기</Button>
+            </DetailBox>
+
+            <DetailBox><AddComments /></DetailBox>
+            <DetailBox><Commentlist /></DetailBox>
+        </>
     )
 }
 
 export default CardDetail;
+
+const DetailBox = styled.div`
+border:1px solid gray;
+border-radius: 15px;
+height: 250px;
+margin-top:30px;
+padding: 30px;
+`
