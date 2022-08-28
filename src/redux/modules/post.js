eab4ee556780576d15b4321d44658e95521df215
limdaeun1 <limdaeun1_@naver.com>
2022-08-29 04:26:42 +0900

    add시 리덕스-서버 저장 구현완료

--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -1,7 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
     cards: [],
+    isLoading: false,
+    error: null,
    };
 
 export const postSlice = createSlice({
@@ -10,7 +13,7 @@ export const postSlice = createSlice({
   reducers: {
     addCard: (state, action) => {
       state.cards.push(action.payload);
-      
+      axios.post("http://localhost:3001/cards", action.payload );
     },
 
     
