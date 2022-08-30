851aad0b53bcf47cc09e1ccb92a0e72b27946bfa
yesol_lee <thfl3363@hanmail.net>
2022-08-30 13:04:11 +0900

    아이디로 상세페이지 이동(클릭 이동X, 상세창 이동)

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -3,27 +3,45 @@ import AddComments from "../comments/AddComments";
 import Commentlist from "../comments/Commentlist";
 import styled from "styled-components";
 import Button from "../elem/Button";
+import Layout from "./Layout";
+import "./CardDetail.css";
+import { useSelector } from "react-redux/es/exports";
+import { useParams } from 'react-router-dom';
 
-function CardDetail() {
+
+function CardDetail( {card}) {  //카드로 프롭스 받음
+
+
+    let state = useSelector((state)=>state)
+
+    let {id} = useParams();
+
+let cardIdx = state.card.find(function(x){ return x.id == id }); 
+
+// let card = {card}
+
+// let cardIdx = card.find(function(x){ return x.id == id }); 
+
     return (
-        <>
-            <Wrapper>
-                <h3>닉네임</h3>
-                <h3>title</h3>
-                <h3>desc</h3>
+        <Layout>
+        <DetailBox>
+            <h3 className="name">닉네임: {cardIdx .name}</h3>
+            <h3 className="title"> title:{cardIdx .title}  </h3>
+            <h3 className="desc">desc:<p>{cardIdx .desc}</p>  </h3>
+            <div className="btnBox">
                 <Button>수정하기</Button>
                 <Button>삭제하기</Button>
-            </Wrapper>
-
-            <Wrapper><AddComments /></Wrapper>
-            <Wrapper><Commentlist /></Wrapper>
-        </>
+            </div>
+        </DetailBox>
+        <DetailBox><Commentlist /></DetailBox>
+        <DetailBox><AddComments /></DetailBox>
+    </Layout>
     )
 }
 
 export default CardDetail;
 
-const Wrapper = styled.div`
+const DetailBox = styled.div`
 border:1px solid gray;
 border-radius: 15px;
 height: 250px;
